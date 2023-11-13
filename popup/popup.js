5ac97fd71e51ea78aae32ab862016bc445f4aacd
hyeon9782 <hyeon9782@gmail.com>
2023-11-14 08:41:02 +0900

    fix: 문법 에러 해결

--- popup/popup.js
+++ popup/popup.js
@@ -2,6 +2,6 @@ const images = Array.from(document.querySelectorAll("img"));
 
 images.forEach((img) => {
   img.addEventListener("click", () => {
-    chrome.storge.local.set({ image: img.src });
+    chrome.storage.local.set({ image: img.src });
   });
 });
